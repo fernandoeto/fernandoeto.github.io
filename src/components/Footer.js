232bd7c539de2cbdbe63a37c4e5954751824b5c9
Fernando Eto <fernando.eto@gmail.com>
2021-09-23 15:30:15 -0300

    Foi atualizado informações de copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export default function Footer(props) {
                     </ul>
 
                     <ul className="copyright">
-                        <li>&copy; Copyright 2020 Mírian Silva</li>
+                        <li>&copy; Copyright 2021 Ciscorp Tecnologia Ltda. ME</li>
                         <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
                     </ul>
 
